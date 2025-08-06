46bc11308bf3c069ad61a527cafa9540001a020b
Siddhraj Thakor <149323330+siddhraj1412@users.noreply.github.com>
2025-08-06 12:58:16 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Sumdemo3 from './Sumdemo3';
 import Week2 from './Week2'
 import Sumdemo from './Sumdemo';
 import Form1 from './Form1';
+import Form2 from './Form2';
 function App() {
   return (<>
     <h1>App</h1>
@@ -16,6 +17,7 @@ function App() {
       <Link to='/Sumdemo3'>Sum2</Link>|
       <Link to='/Sumdemo'>Sum3</Link>|
       <Link to='/Form1'>Form1</Link>|
+      <Link to='/Form2'>Form2</Link>|
       <Routes>
         <Route path='/' element={<Week2 />} />
         <Route path='/Week2' element={<Week2 />} />|
@@ -24,6 +26,7 @@ function App() {
         <Route path='/Sumdemo3' element={<Sumdemo3 />} />
         <Route path='/Sumdemo' element={<Sumdemo />} />
         <Route path='/Form1' element={<Form1 />} />
+        <Route path='/Form2' element={<Form2 />} />
         <Route path='/cake/ahmedabad' element={<h1>Ahmedabad Cake </h1>} />
         <Route path='/cake/surat' element={<h1>Surat Cake</h1>} />
         <Route path='*' element={<h1>404 File Not Found</h1>} />
